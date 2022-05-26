76d97c3281084be3b7aff251f4123ccf6c43a1ea
rezashakeri <rezashakeri2648@gmail.com>
2022-05-26 21:48:42 +0430

    feat(add follow and unfollow): add follow and unfollow view and template ✅
    1. add follow and unfollow view
    2. add follow and unfollow template

--- user_profile/views.py
+++ user_profile/views.py
@@ -2,23 +2,42 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
 from django.shortcuts import render, redirect
 
-from user_management.models import User
+from user_profile.models import Profile
 
 
 def profile_view(request, username):
     # We check if the user is logged in?
     if request.user.is_authenticated:
         try:
-            # get user by username
-            current_user = User.objects.get(username__iexact=username)
+            # get user profile by username
+            profile = Profile.objects.get(user__username__iexact=username)
         # if user not found raise 404 error
         except ObjectDoesNotExist:
             raise Http404
-
+        # if request methods are POST
+        if request.method == 'POST':
+            # get current user profile
+            current_user_profile = request.user.profile
+            # get request data
+            data = request.POST
+            # get request data with follow name
+            action = data.get('follow')
+            # if the value of follow is follow
+            if action == 'follow':
+                # add profile to follows table
+                current_user_profile.follows.add(profile)
+            # if the value of follow is unfollow
+            elif action == 'unfollow':
+                # remove profile from follows table
+                current_user_profile.follows.remove(profile)
+            # save user data
+            current_user_profile.save()
+        # context data
         context = {
-            'user': current_user
+            'profile': profile
         }
         return render(request, 'user_profile/profile.html', context)
+
     # if a user is not login in redirecting to login page
     else:
         return redirect('account_login')
