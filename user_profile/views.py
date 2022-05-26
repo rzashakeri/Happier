f23142c6647a6179802335be4f684c99b358c6cb
rezashakeri <rezashakeri2648@gmail.com>
2022-05-26 04:42:01 +0430

    refactor(improve profile view logic): improve profile view logic and error handling ✅

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,14 +1,24 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 from django.shortcuts import render, redirect
 
 from user_management.models import User
 
 
 def profile_view(request, username):
+    # We check if the user is logged in?
     if request.user.is_authenticated:
-        current_user = User.objects.get(username__iexact=username)
+        try:
+            # get user by username
+            current_user = User.objects.get(username__iexact=username)
+        # if user not found raise 404 error
+        except ObjectDoesNotExist:
+            raise Http404
+
         context = {
             'user': current_user
         }
         return render(request, 'user_profile/profile.html', context)
+    # if a user is not login in redirecting to login page
     else:
         return redirect('account_login')
