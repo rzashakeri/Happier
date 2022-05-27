e22a45071a8d5d7061877a03a5a61bf8365fe6c9
rezashakeri <rezashakeri2648@gmail.com>
2022-05-28 02:57:57 +0430

    feat(Custom Add Email Form): customize add email form for email management section ✅

--- user_management/forms.py
+++ user_management/forms.py
@@ -1,4 +1,4 @@
-from allauth.account.forms import SignupForm, LoginForm, ResetPasswordForm, ResetPasswordKeyForm
+from allauth.account.forms import SignupForm, LoginForm, ResetPasswordForm, ResetPasswordKeyForm, AddEmailForm
 
 
 class CustomSignupForm(SignupForm):
@@ -23,8 +23,14 @@ class CustomResetPasswordForm(ResetPasswordForm):
         self.fields['email'].widget.attrs['class'] = 'block w-32 input input-bordered max-w-xs'
 
 
-class CustomResetPasswordFromKey(ResetPasswordKeyForm):
+class CustomResetPasswordFromKeyForm(ResetPasswordKeyForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['password1'].widget.attrs['class'] = 'block w-full mt-2 input input-bordered w-full max-w-xs'
         self.fields['password2'].widget.attrs['class'] = 'block w-full mt-2 input input-bordered w-full max-w-xs'
+
+
+class CustomAddEmailForm(AddEmailForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['class'] = 'input input-bordered w-full max-w-xs mt-2'
