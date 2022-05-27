07c4a507ab514cb612eb660a708c98c674af8297
rezashakeri <rezashakeri2648@gmail.com>
2022-05-28 03:27:53 +0430

    feat(header component view): add header component view for render partial ✅

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from user_management.forms import CustomSigninForm
+from user_management.models import User
 
 
 def home(request):
@@ -9,3 +10,11 @@ def home(request):
         'login_form': login_form
     }
     return render(request, 'home/home_layout.html', context)
+
+
+def header_component(request):
+    user = User.objects.get(id=request.user.id)
+    context = {
+        'user': user
+    }
+    return render(request, 'components/header_component.html', context)
