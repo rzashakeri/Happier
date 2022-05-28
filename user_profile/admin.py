f1237c16436970baf418a9828d1dfeb67d7b9fa7
rezashakeri <rezashakeri2648@gmail.com>
2022-05-28 17:07:12 +0430

    feat(add profile to admin): show user profile data (full name or username) in admin panel ✅

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
+
+admin.site.register(Profile, ProfileAdmin)
