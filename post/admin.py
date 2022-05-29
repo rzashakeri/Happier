7033f5c095869479200ac73d6cc5af79bb3eefb7
rezashakeri <rezashakeri2648@gmail.com>
2022-05-29 06:45:54 +0430

    feat(add Post Model to admin ): add models for show in admin ✅

--- post/admin.py
+++ post/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Post, PostComment, PostCategory
+
+admin.site.register(Post)
+admin.site.register(PostComment)
+admin.site.register(PostCategory)
