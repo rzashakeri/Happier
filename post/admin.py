467e36f2be24fefdbb9a66b5c1080195c56b9daa
rezashakeri <rezashakeri2648@gmail.com>
2022-06-05 17:30:57 +0430

    refactor(delete post from admin)

--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Post, PostComment, PostCategory
+from .models import PostComment, PostCategory
 
-admin.site.register(Post)
 admin.site.register(PostComment)
 admin.site.register(PostCategory)
