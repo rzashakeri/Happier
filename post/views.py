5619bd458ae3a0ec84ada7d9ab099f8303aa1364
rezashakeri <rezashakeri2648@gmail.com>
2022-06-05 17:27:01 +0430

    feat(upload post attachment): add upload post attachment view and logic (image, video, audio)✅

--- post/views.py
+++ post/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render
 
-from post.forms import UploadPostForm
+from post.forms import UploadPostForm, UploadPostAttachmentForm
 
 
 def upload_post_view(request):
@@ -9,10 +9,14 @@ def upload_post_view(request):
     if request.user.is_authenticated:
         # get upload post form
         upload_post_form = UploadPostForm()
+        upload_post_attachment_form = UploadPostAttachmentForm()
         # get Post request
         if request.method == "POST":
             # Initialization upload post form
-            upload_post_form = UploadPostForm(request.POST, request.FILES)
+            upload_post_form = UploadPostForm(request.POST)
+            upload_post_attachment_form = UploadPostAttachmentForm(
+                request.POST, request.FILES
+            )
             # Is the reviewed upload form valid?
             if upload_post_form.is_valid():
                 # gets model object, and add user data and save
@@ -21,12 +25,25 @@ def upload_post_view(request):
                 upload_post.user = request.user
                 # after pass user id form has saved
                 upload_post.save()
+                post_id = upload_post.id
+                if upload_post_attachment_form.data:
+                    if upload_post_attachment_form.is_valid():
+                        attachment = upload_post_attachment_form.save(commit=False)
+                        attachment.post_id = post_id
+                        attachment.save()
+
             # get upload form and pass to context
-            context = {"upload_post_form": upload_post_form}
+            context = {
+                "upload_post_form": upload_post_form,
+                "upload_post_attachment_form": upload_post_attachment_form,
+            }
             # return request and template with context when request method is post
             return render(request, "post/upload_post.html", context)
         # return request and template with context when request method is get
-        context = {"upload_post_form": upload_post_form}
+        context = {
+            "upload_post_form": upload_post_form,
+            "upload_post_attachment_form": upload_post_attachment_form,
+        }
         return render(request, "post/upload_post.html", context)
     else:
         # if a user isn't authenticated, show not found(404) page
