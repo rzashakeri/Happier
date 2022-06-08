21931044a39e454e14bd9ebe67a91545c17efe7c
rezashakeri <rezashakeri2648@gmail.com>
2022-06-08 22:29:33 +0430

    feat(add profile urls):
    1. add personal information url
    2. add delete account url

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 
-from .views import profile_view, edit_profile_view
+from .views import (
+    profile_view,
+    edit_profile_view,
+    delete_account_view,
+    edit_personal_information,
+)
 
 urlpatterns = [
-    path('<str:username>/', profile_view, name='profile'),
-    path('accounts/edit', edit_profile_view, name='edit_profile')
+    path("<str:username>/", profile_view, name="profile"),
+    path("accounts/edit/profile", edit_profile_view, name="edit_profile"),
+    path(
+        "accounts/edit/personal-information",
+        edit_personal_information,
+        name="edit_personal_information",
+    ),
+    path("accounts/delete", delete_account_view, name="delete_account"),
 ]
