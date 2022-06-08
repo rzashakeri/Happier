7338fd8f51b187ee5b5fc9256efba5b76829c302
rezashakeri <rezashakeri2648@gmail.com>
2022-06-08 22:33:46 +0430

    feat(add tailwind css screen breakpoint to tailwind.config.js)

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -4,11 +4,44 @@ module.exports = {
         '../feed/**/*.{html,js}',
         '../home/**/*.{html,js}',
         '../user_profile/**/*.{html,js}',
-        '../static/js/**/*.{html,js, json}'
+        '../static/**/*.{html,js}',
+        '../post/**/*.{html,js}',
+        '../venv/Lib/crispy_tailwind/**/*.{html,js}',
+
     ],
     darkMode: 'data-theme',
     theme: {
         extend: {},
+        screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+            'max-2xl': {'max': '1535px'},
+            // => @media (max-width: 1535px) { ... }
+
+            'max-xl': {'max': '1279px'},
+            // => @media (max-width: 1279px) { ... }
+
+            'max-lg': {'max': '1023px'},
+            // => @media (max-width: 1023px) { ... }
+
+            'max-md': {'max': '767px'},
+            // => @media (max-width: 767px) { ... }
+
+            'max-sm': {'max': '639px'},
+            // => @media (max-width: 639px) { ... }
+        }
     },
     plugins: [require("daisyui"), require('@tailwindcss/line-clamp')],
     daisyui: {
