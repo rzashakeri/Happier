f59977e87cf161253bb69476f574f2e9fd16295b
rezashakeri <rezashakeri2648@gmail.com>
2022-06-09 01:45:29 +0430

    refactor(change user model):
    1. delete field in the model:
    .profile image
    .biography
    .birthday

--- user_management/models.py
+++ user_management/models.py
@@ -1,23 +1,10 @@
-from birthday import BirthdayField, BirthdayManager
-from constrainedfilefield.fields import ConstrainedImageField
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 
 # custom user model
 class User(AbstractUser):
     phone_number = PhoneNumberField(blank=True)
-    profile_image = ConstrainedImageField(
-        upload_to="profile",
-        null=True,
-        blank=True,
-        content_types=["image/png", "image/jpeg"],
-        max_upload_size=10485760,
-    )
-    biography = models.CharField(max_length=200, null=True, blank=True)
-    birthday = BirthdayField()
-    objects = BirthdayManager()
 
     # show user data in admin when UserAdmin class in admin not set
     def __str__(self):
