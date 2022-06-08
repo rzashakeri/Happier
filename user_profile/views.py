f02c5a68f59d0c73a030e03f12f18b304f37fc3c
rezashakeri <rezashakeri2648@gmail.com>
2022-06-08 22:24:13 +0430

    feat(add edit personal information view and delete account view):
    1. add edit personal information view
    this view to showing personal data like to name, family, username, email
    2. add delete account view
    this view for delete account

--- user_profile/views.py
+++ user_profile/views.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
 from django.shortcuts import render, redirect
 
-from user_profile.forms import EditProfileForm
+from user_profile.forms import EditPersonalInformationForm, EditProfileForm
 from user_profile.models import Profile
 
 
@@ -19,63 +19,110 @@ def profile_view(request, username):
         except ObjectDoesNotExist:
             raise Http404
         # if request methods are POST
-        if request.method == 'POST':
+        if request.method == "POST":
             # get current user profile
             current_user_profile = request.user.profile
             # get request data
             data = request.POST
             # get request data with follow name
-            action = data.get('follow')
+            action = data.get("follow")
             # if the value of follow is follow
-            if action == 'follow':
+            if action == "follow":
                 # add profile to follows table
                 current_user_profile.follows.add(profile)
             # if the value of follow is unfollow
-            elif action == 'unfollow':
+            elif action == "unfollow":
                 # remove profile from follows table
                 current_user_profile.follows.remove(profile)
             # save user data
             current_user_profile.save()
         # context data
-        context = {
-            'profile': profile,
-            'profiles': profiles
-        }
-        return render(request, 'user_profile/profile.html', context)
+        context = {"profile": profile, "profiles": profiles}
+        return render(request, "user_profile/profile.html", context)
 
     # if a user is not login in redirecting to login page
     else:
-        return redirect('account_login')
+        return redirect("account_login")
 
 
 @verified_email_required
-def edit_profile_view(request):
+def edit_personal_information(request):
     # checked user is authenticated
     if request.user.is_authenticated:
         # get the edit profile model form
-        profile_form = EditProfileForm(instance=request.user)
+        profile_form = EditPersonalInformationForm(instance=request.user)
         # check request is post?
-        if request.method == 'POST':
+        if request.method == "POST":
             # send user data to edit profile form
-            profile_form = EditProfileForm(request.POST, request.FILES, instance=request.user)
+            profile_form = EditPersonalInformationForm(
+                request.POST, instance=request.user
+            )
             # check user profile form is valid
             if profile_form.is_valid():
                 # if user profile is valid, save the form
                 profile_form.save()
                 # show success message when save user data
-                messages.success(request, 'Profile saved')
+                messages.success(request, "Profile saved")
             # put edit profile form with context and send to template
-            context = {
-                'profile_form': profile_form
-            }
+            context = {"profile_form": profile_form}
             # render request and template and context
-            return render(request, 'user_profile/edit_profile.html', context)
+            return render(
+                request, "user_profile/edit_personal_information.html", context
+            )
             # pass context
-        context = {
-            'profile_form': profile_form
-        }
+        context = {"profile_form": profile_form}
         # return request and template and context
-        return render(request, 'user_profile/edit_profile.html', context)
+        return render(request, "user_profile/edit_personal_information.html", context)
     # if a user is not authenticated, redirect to login page
     else:
-        return redirect(reversed('account_login'))
+        return redirect(reversed("account_login"))
+
+
+def edit_profile_view(request):
+    # checked user is authenticated
+    if request.user.is_authenticated:
+        # get the edit profile model form
+        profile_form = EditProfileForm(instance=request.user.profile)
+        # check request is post?
+        if request.method == "POST":
+            # send user data to edit profile form
+            profile_form = EditProfileForm(
+                request.POST, request.FILES, instance=request.user.profile
+            )
+            # check user profile form is valid
+            if profile_form.is_valid():
+                # if user profile is valid, save the form
+                profile_form.save()
+                # show success message when save user data
+                messages.success(request, "Profile saved")
+            # put edit profile form with context and send to template
+            context = {"profile_form": profile_form}
+            # render request and template and context
+            return render(request, "user_profile/edit_profile.html", context)
+            # pass context
+        context = {"profile_form": profile_form}
+        # return request and template and context
+        return render(request, "user_profile/edit_profile.html", context)
+    # if a user is not authenticated, redirect to login page
+    else:
+        return redirect(reversed("account_login"))
+
+
+def delete_account_view(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            data = request.POST.get("delete")
+            if data == "yes":
+                user = request.user
+                user.delete()
+                user.save()
+                messages.success(
+                    request,
+                    "Your account has been successfully deleted, We hope you will be back soon :)",
+                )
+                return redirect("home")
+            else:
+                return redirect("home")
+        return render(request, "user_profile/delete_account.html")
+    else:
+        return redirect("account_login")
