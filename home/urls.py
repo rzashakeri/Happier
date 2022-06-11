acc498fc5843ff2c4e240811b3cb5cb3028cd381
rezashakeri <rezashakeri2648@gmail.com>
2022-06-11 22:51:56 +0430

    refactor: change '' to "" in url name

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 
-from .views import home
+from home.views import home
 
-urlpatterns = [
-    path('', home, name='home')
-]
+urlpatterns = [path("", home, name="home")]
