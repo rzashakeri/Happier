c33d036537c1419e51c9d7be3c0f94f7e8ddcef5
rezashakeri <rezashakeri2648@gmail.com>
2022-06-11 22:41:58 +0430

    refactor: delete upload post form

--- post/forms.py
+++ post/forms.py
@@ -1,29 +1,3 @@
-from django import forms
-
-from post.models import Post, PostAttachment
 
 
-class UploadPostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ["caption"]
-        widgets = {
-            "caption": forms.Textarea(
-                attrs={
-                    "class": "textarea textarea-bordered w-full",
-                    "placeholder": "Create a Post",
-                    "rows": "4",
-                }
-            )
-        }
 
-
-class UploadPostAttachmentForm(forms.ModelForm):
-    class Meta:
-        model = PostAttachment
-        fields = ["image", "audio", "video"]
-        widgets = {
-            "audio": forms.FileInput(attrs={"accept": "audio/*"}),
-            "video": forms.FileInput(attrs={"accept": "video/*"}),
-            "image": forms.FileInput(attrs={"accept": "image/*"}),
-        }
