f8f8c1cb545743366ab13cab4185b0590922a062
rezashakeri <rezashakeri2648@gmail.com>
2022-06-11 22:28:27 +0430

    feat: add ImagePost, VoicePost, VideoPost, TextPost To Admin

--- post/admin.py
+++ post/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import PostComment, PostCategory
+from .models import VoicePost, ImagePost, VideoPost, TextPost, PostCategory
 
-admin.site.register(PostComment)
+admin.site.register(ImagePost)
+admin.site.register(VoicePost)
+admin.site.register(VideoPost)
+admin.site.register(TextPost)
 admin.site.register(PostCategory)
