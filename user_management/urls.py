91d2828639bbf1220a0cf05c9986fae3ee75e25a
rezashakeri <rezashakeri2648@gmail.com>
2022-06-17 22:27:18 +0430

    feat: adding SignupWizard url for separate step signup form

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,3 +1,26 @@
-urlpatterns = [
+from django.urls import path
 
+from .forms import (
+    SignupFormStepOne,
+    SignupFormStepTwo,
+    SignupFormStepThree,
+    SignupFormStepFour,
+    SignupFormStepFive,
+)
+from .views import SignupWizard
+
+# Register User Separate steps
+urlpatterns = [
+    path(
+        "register/",
+        SignupWizard.as_view(
+            [
+                SignupFormStepOne,
+                SignupFormStepTwo,
+                SignupFormStepThree,
+                SignupFormStepFour,
+                SignupFormStepFive,
+            ]
+        ),
+    ),
 ]
