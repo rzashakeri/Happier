a17e45156dc8162271f4f79f1f338f9fb266125d
rezashakeri <rezashakeri2648@gmail.com>
2022-06-17 22:25:09 +0430

    feat: adding SignupWizard view for the separate step signup form

---
+++ user_management/views.py
@@ -0,0 +1,43 @@
+from allauth.account.utils import complete_signup
+from django.shortcuts import render
+from formtools.wizard.views import SessionWizardView
+
+from config import settings
+from .forms import (
+    SignupFormStepOne,
+    SignupFormStepTwo,
+    SignupFormStepThree,
+    SignupFormStepFour,
+    SignupFormStepFive,
+)
+from .models import User
+
+
+# Register User Separate steps
+class SignupWizard(SessionWizardView):
+    form_list = [
+        SignupFormStepOne,
+        SignupFormStepTwo,
+        SignupFormStepThree,
+        SignupFormStepFour,
+        SignupFormStepFive,
+    ]
+
+    def done(self, form_list, **kwargs):
+        first_name = form_list[0].cleaned_data["first_name"]
+        last_name = form_list[1].cleaned_data["last_name"]
+        username = form_list[2].cleaned_data["username"]
+        email = form_list[3].cleaned_data["email"]
+        password = form_list[4].cleaned_data["password1"]
+        user = User(
+            first_name=first_name, last_name=last_name, username=username, email=email
+        )
+        user.set_password(password)
+        user.save()
+        complete_signup(
+            self.request,
+            user,
+            settings.ACCOUNT_EMAIL_VERIFICATION,
+            settings.LOGIN_REDIRECT_URL,
+        )
+        return render(self.request, "account/verification_sent.html")
