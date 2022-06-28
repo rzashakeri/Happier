c298bf5cf7d29d8d552bcaad91820816a8d3cf40
rezashakeri <rezashakeri2648@gmail.com>
2022-06-29 00:20:54 +0430

    feat: implement post, like, comment url

--- post/urls.py
+++ post/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from .views import like_view, post_view, create_comment_view
+
+urlpatterns = [
+    path("like/<int:post_id>/", like_view, name="like"),
+    path("<slug:slug>/", post_view, name="show_post"),
+    path("comment/create/<int:post_id>/", create_comment_view, name="create_comment"),
+]
