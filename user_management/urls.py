cf841cd989aabd7910c5ed8dc5c4bed5d814b577
rezashakeri <rezashakeri2648@gmail.com>
2022-06-29 00:21:56 +0430

    refactor: deleted url for Register User in Separate steps

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,26 +1,2 @@
-from django.urls import path
-
-from .forms import (
-    SignupFormStepOne,
-    SignupFormStepTwo,
-    SignupFormStepThree,
-    SignupFormStepFour,
-    SignupFormStepFive,
-)
-from .views import SignupWizard
-
 # Register User Separate steps
-urlpatterns = [
-    path(
-        "register/",
-        SignupWizard.as_view(
-            [
-                SignupFormStepOne,
-                SignupFormStepTwo,
-                SignupFormStepThree,
-                SignupFormStepFour,
-                SignupFormStepFive,
-            ]
-        ),
-    ),
-]
+urlpatterns = []
