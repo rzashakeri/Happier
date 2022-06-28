a0f937178734cdaadf3f6c74d3978643623ed25c
rezashakeri <rezashakeri2648@gmail.com>
2022-06-29 00:29:37 +0430

    feat: adding video, image, like model to admin

--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Post, PostAttachment, Audio
+from .models import Post, Audio, Video, Image, Like
 
 admin.site.register(Post)
-admin.site.register(PostAttachment)
 admin.site.register(Audio)
+admin.site.register(Video)
+admin.site.register(Image)
+admin.site.register(Like)
