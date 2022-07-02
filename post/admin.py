d9bad0766b3ca16956f543fb818ec2424be2b6af
rezashakeri <rezashakeri2648@gmail.com>
2022-07-02 04:50:36 +0430

    refactor: change name like model to postlike model in admin

--- post/admin.py
+++ post/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import Post, Audio, Video, Image, Like
+from .models import Post, Audio, Video, Image, PostLike
 
 admin.site.register(Post)
 admin.site.register(Audio)
 admin.site.register(Video)
 admin.site.register(Image)
-admin.site.register(Like)
+admin.site.register(PostLike)
