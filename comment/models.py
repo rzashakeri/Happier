1e276ab33e47beda9b197e0c6a5f769350ad730e
rezashakeri <rezashakeri2648@gmail.com>
2022-07-02 05:46:11 +0430

    feat: adding comment model

---
+++ comment/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from post.models import Post
+from user_management.models import User
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent = models.ForeignKey(
+        "Comment", on_delete=models.CASCADE, null=True, blank=True
+    )
+
+
+class CommentLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
