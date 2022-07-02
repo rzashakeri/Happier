c6d1e703ed84a1c57ed0f3577086e4f8031ca50d
rezashakeri <rezashakeri2648@gmail.com>
2022-07-02 04:57:39 +0430

    refactor: adding time_format template tag For the amount of time it took to upload the post with this format: 11 Feb 2011 16:29:00 becomes a minute ago or 17 Feb 2017 13:41:29 becomes 2 hours ago. and show in post

--- templatetag/templatetags/custom_template_tag.py
+++ templatetag/templatetags/custom_template_tag.py
@@ -1,4 +1,5 @@
 from django import template
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 register = template.Library()
 
@@ -6,3 +7,19 @@ register = template.Library()
 @register.filter
 def minus(value):
     return value - 1
+
+
+@register.filter
+def time_format(value):
+    received_time = naturaltime(value).split(",")[0]
+    print(received_time)
+    if "year" in received_time:
+        return value.strftime("%d %b %Y")
+    elif "month" in received_time:
+        return value.strftime("%d %b %Y")
+    elif "week" in received_time:
+        return value.strftime("%d %b %Y")
+    elif "day" in received_time:
+        return value.strftime("%d %b %Y")
+    else:
+        return received_time
