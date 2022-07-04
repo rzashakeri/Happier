388cb1c8ebd133bea5d25782010571d769ea44df
rezashakeri <rezashakeri2648@gmail.com>
2022-07-04 23:57:48 +0430

    refactor: submit comment use ajax

--- static/js/comment/create_comment.js
+++ static/js/comment/create_comment.js
@@ -1,28 +1,33 @@
-$('#comment-form').on("submit", CreateComment);
-
-function CreateComment(event) {
-    event.preventDefault();
-    let PostId = $("#comment-post-id").val();
-    let CommentText = $("#comment-text").val();
-    $.ajax({
-        url: "/comment/create/" + PostId + "/",
-        method: "POST",
-        data: {post_id: PostId, text: CommentText},
-
-        success: function (json) {
-            if (json.result === "NOK") {
-                $("#comment-form").addClass("border-2 border-rose-600 rounded-lg");
-            } else if (json.result === "OK") {
-                $("#comment-count").text(json.count);
-                console.log(json)
-                $("#comment-text").val("");
-            }
-        },
-
-        error: function (json) {
-            console.log('faild')
-            console.log(json)
-        }
-
-    });
-}
+window.addEventListener("load", function () {
+    let Post = document.querySelectorAll("#post");
+    if (Post != null) {
+        Post.forEach(Post => {
+            let CommentForm = Post.querySelector("#comment-form");
+            let CommentCount = Post.querySelector("#comment-count");
+            let CommentText = Post.querySelector("#comment-text");
+            console.log()
+            CommentForm.addEventListener("submit", function (event) {
+                event.preventDefault();
+                let PostId = CommentForm.querySelector("#comment-post-id").value;
+                let CommentText = CommentForm.querySelector("#comment-text").value;
+                $.ajax({
+                    url: "/comment/create/" + PostId + "/",
+                    method: "POST",
+                    data: {post_id: PostId, text: CommentText},
+                    success: function (json) {
+                        if (json.result === "NOK") {
+                            CommentForm.classList.add("border-2 border-rose-600 rounded-lg");
+                        } else if (json.result === "OK") {
+                            CommentCount.textContent = json.count;
+                            CommentForm.querySelector("#comment-text").value = "";
+                        }
+                    },
+                    error: function (json) {
+                        console.log('faild')
+                        console.log(json)
+                    }
+                });
+            })
+        })
+    }
+})
