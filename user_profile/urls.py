6a51d4e4a32d2ac1ff1f69e6e309fe9b45e13784
rezashakeri <rezashakeri2648@gmail.com>
2022-07-08 05:32:06 +0430

    feat: adding follow request url

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -7,6 +7,7 @@ from .views import (
     edit_personal_information,
     followers,
     following,
+    follow_request,
 )
 
 urlpatterns = [
@@ -20,4 +21,9 @@ urlpatterns = [
     path("accounts/delete", delete_account_view, name="delete_account"),
     path("<str:username>/followers", followers, name="followers"),
     path("<str:username>/following", following, name="following"),
+    path(
+        "follow/request/<str:username>/",
+        follow_request,
+        name="follow_request",
+    ),
 ]
