de9a256adb126c5c27c408fb3e290e787b34f613
rezashakeri <rezashakeri2648@gmail.com>
2022-07-09 21:48:20 +0430

    feat: adding default value to is_private field

--- user_profile/models.py
+++ user_profile/models.py
@@ -40,7 +40,7 @@ class Profile(models.Model):
     )
     biography = models.CharField(max_length=200, null=True, blank=True)
     job = models.CharField(max_length=20, null=True, blank=True)
-    is_private = models.BooleanField()
+    is_private = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
