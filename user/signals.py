ba4c054f9ddcbd72fb8e55371b92602d9fb9bff3
rezashakeri <rezashakeri2648@gmail.com>
2022-07-22 20:59:42 +0430

    refactor: adding follow function to signals.py in user app

--- user/signals.py
+++ user/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from actstream.actions import follow, unfollow
 from user.models import User
 from .models import Profile
 
@@ -10,5 +10,4 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
         user_profile.save()
-        user_profile.follows.add(instance.profile)
-        user_profile.save()
+        follow(instance, instance)
