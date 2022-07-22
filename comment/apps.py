c67e52c424f2d1f85f26ecb579659c309792dc36
rezashakeri <rezashakeri2648@gmail.com>
2022-07-22 21:51:27 +0430

    test: importing comment signals to apps.py

--- comment/apps.py
+++ comment/apps.py
@@ -2,5 +2,11 @@ from django.apps import AppConfig
 
 
 class CommentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'comment'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "comment"
+
+    def ready(self):
+        from actstream import registry
+
+        registry.register(self.get_model("Comment"))
+        import comment.signals
