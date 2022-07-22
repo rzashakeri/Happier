a3599941c6844f4b277d3000cc83237ebcee2c98
rezashakeri <rezashakeri2648@gmail.com>
2022-07-22 21:51:57 +0430

    test: importing post signals to apps.py

--- post/apps.py
+++ post/apps.py
@@ -2,5 +2,12 @@ from django.apps import AppConfig
 
 
 class PostConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'post'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "post"
+
+    def ready(self):
+        from actstream import registry
+
+        registry.register(self.get_model("Post"))
+        registry.register(self.get_model("PostLike"))
+        import post.signals
