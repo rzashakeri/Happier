e945b3a922edf3653b34bb46efbc4be0c2129fea
rezashakeri <rezashakeri2648@gmail.com>
2022-07-22 20:57:03 +0430

    feat: adding test for models.py in user app

--- user/tests/test_models.py
+++ user/tests/test_models.py
@@ -1,22 +1,11 @@
 import pytest
-
 from user.models import Profile
 from user.models import User
+from .fixtures import user, user_without_full_name
 
 
 @pytest.mark.django_db
-class TestUserProfile:
-    @pytest.fixture
-    def user(self):
-        user, created = User.objects.get_or_create(
-            first_name="test_first_name",
-            last_name="test_last_name",
-            username="test_username",
-            email="test@test.com",
-            is_active=True,
-            is_superuser=False,
-        )
-        return user
+class TestUserModel:
 
     def test_whether_profile_is_created(self, user):
         profile, created = Profile.objects.get_or_create(user)
@@ -26,3 +15,21 @@ class TestUserProfile:
         assert profile.user.email == "test@test.com"
         assert profile.user.is_active
         assert profile.user.is_superuser is False
+
+    def test_user_str_method_return_username(self, user_without_full_name):
+        assert user_without_full_name.__str__() == "test_username"
+
+
+@pytest.mark.django_db
+class TestProfileModel:
+
+    def test_profile_str_method_return_true_username(self, user):
+        assert user.profile.__str__() == "test_username"
+
+    def test_profile_get_absolute_url_method_return_true_address(
+            self, user, client
+    ):
+        client.force_login(user)
+        response = client.get(user.profile.get_absolute_url())
+        assert user.profile.get_absolute_url
+        assert response.status_code == 200
