1c08aea44c923a963c8a8a13e3be96592814f7a8
rezashakeri <rezashakeri2648@gmail.com>
2022-07-22 20:51:54 +0430

    refactor: add user following list to feed views.py

--- feed/views.py
+++ feed/views.py
@@ -1,7 +1,10 @@
 from allauth.account.decorators import verified_email_required
 from django.shortcuts import render
+from actstream.models import following, followers
 
 
 @verified_email_required
 def feed(request):
-    return render(request, "feed/feed.html")
+    user_followings = following(request.user)
+    context = {"followings": user_followings}
+    return render(request, "feed/feed.html", context)
