1fce13328648d641a8f2ba265cadba4b1015871e
rezashakeri <rezashakeri2648@gmail.com>
2022-07-26 00:03:07 +0430

    test: adding user factory to test_models.py 1. adding email_confirmation function for configuration user email 2. changing value of user first_name, last_name, username, email

--- user/tests/test_models.py
+++ user/tests/test_models.py
@@ -1,34 +1,42 @@
 import pytest
 from user.models import Profile
 from user.models import User
-from .fixtures import user, user_without_full_name
+from .fixtures import UserFactory, email_confirmation
 
 
 @pytest.mark.django_db
 class TestUserModel:
 
-    def test_whether_profile_is_created(self, user):
+    def test_whether_profile_is_created(self):
+        user = UserFactory()
+        email_confirmation(user)
         profile, created = Profile.objects.get_or_create(user)
-        assert profile.user.first_name == "test_first_name"
-        assert profile.user.last_name == "test_last_name"
-        assert profile.user.username == "test_username"
-        assert profile.user.email == "test@test.com"
+        assert profile.user.first_name == "reza"
+        assert profile.user.last_name == "shakeri"
+        assert profile.user.username == "rzashakeri"
+        assert profile.user.email == "rezashakeri@test.com"
         assert profile.user.is_active
         assert profile.user.is_superuser is False
 
-    def test_user_str_method_return_username(self, user_without_full_name):
-        assert user_without_full_name.__str__() == "test_username"
+    def test_user_str_method_return_username(self):
+        user = UserFactory(first_name="", last_name="")
+        email_confirmation(user)
+        assert user.__str__() == "rzashakeri"
 
 
 @pytest.mark.django_db
 class TestProfileModel:
 
-    def test_profile_str_method_return_true_username(self, user):
-        assert user.profile.__str__() == "test_username"
+    def test_profile_str_method_return_true_username(self):
+        user = UserFactory()
+        email_confirmation(user)
+        assert user.profile.__str__() == "rzashakeri"
 
     def test_profile_get_absolute_url_method_return_true_address(
-            self, user, client
+            self, client
     ):
+        user = UserFactory()
+        email_confirmation(user)
         client.force_login(user)
         response = client.get(user.profile.get_absolute_url())
         assert user.profile.get_absolute_url
