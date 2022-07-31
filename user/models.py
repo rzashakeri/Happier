42115cc66bc718ae9853bfbb37b29d03ee258d11
rezashakeri <rezashakeri2648@gmail.com>
2022-08-01 02:15:02 +0430

    feat: adding show_welcome_message field to the profile model

--- user/models.py
+++ user/models.py
@@ -53,6 +53,7 @@ class Profile(models.Model):
     biography = models.CharField(max_length=200, null=True, blank=True)
     job = models.CharField(max_length=20, null=True, blank=True)
     is_private = models.BooleanField(default=False)
+    show_welcome_message = models.BooleanField(default=True)
 
     def __str__(self):
         return self.user.username
