3aa2e0446f05ee11b00629a14a75fa7af114373b
rezashakeri <rezashakeri2648@gmail.com>
2022-08-01 02:21:43 +0430

    refactor: deleting unused codes (*args, **kwargs)

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 
-# from post.forms import UploadPostAttachmentForm, UploadPostForm
 from user.forms import CustomSigninForm
 from user.models import User
 
@@ -14,7 +13,7 @@ def home(request):
         return render(request, "home/home_layout.html", context)
 
 
-def header_component(request, *args, **kwargs):
+def header_component(request):
     """
     checked if user is not logged in set user is None,
     And if we do not do this, we will encounter a render partial error
