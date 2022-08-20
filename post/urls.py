fff930a639c4df15bb9a8fe043cc560cecbac565
rezashakeri <rezashakeri2648@gmail.com>
2022-08-20 21:32:23 +0430

    refactor: adding create post url

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import like_view, post_view, show_post_view
+from .views import like_view, post_view, show_post_view, create_new_post_view
 
 urlpatterns = [
     path("like/<int:post_id>/", like_view, name="like"),
     path("<int:post_id>/", post_view, name="post"),
     path("<slug:slug>", show_post_view, name="show_post"),
+    path("create/new/post", create_new_post_view, name="upload_post"),
 ]
