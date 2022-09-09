7a8c1916d7ee2d4846f63d6cacfeda2a86fe542c
Reza Shakeri <rzashakeri@outlook.com>
2022-09-10 01:00:46 +0430

    feat: adding post app urls

--- post/urls.py
+++ post/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import like_view, post_view, show_post_view, create_new_post_view
 
 urlpatterns = [
-    path("like/<int:post_id>/", like_view, name="like"),
-    path("<int:post_id>/", post_view, name="post"),
-    path("<slug:slug>", show_post_view, name="show_post"),
-    path("create/new/post", create_new_post_view, name="upload_post"),
+    path("post/like/<int:post_id>/", like_view, name="like"),
+    path("post/<int:post_id>/", post_view, name="post"),
+    path("post/<slug:slug>", show_post_view, name="show_post"),
+    path("create/new/post/", create_new_post_view, name="upload_post"),
 ]
